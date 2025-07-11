ebf0b71bc900ab2961b9dee3c3d8010615bef6b8
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-11 16:16:49 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -5,13 +5,33 @@ const connectDB = require('./config/db');
 
 dotenv.config();
 
-// Call the function
+// Connect to MongoDB
 connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middleware to parse JSON
+app.use(express.json());
 
+// Import Routes
+const billRoutes = require('./routes/billsRoute');
+// Add more routes later: tenantRoutes, landlordRoutes etc.
+
+// Use Routes
+app.use('/api/bills', billRoutes);
+
+
+const tenantRoutes = require('./routes/tenantRoute');
+app.use('/api/tenants', tenantRoutes);
+
+
+// Test Route (optional)
+app.get('/', (req, res) => {
+  res.send('Home Management Backend Running');
+});
+
+// Server Listen
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
