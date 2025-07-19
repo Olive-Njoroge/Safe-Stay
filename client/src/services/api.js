4aba05df79cd0b7338eb127483c855a6ac27b3d8
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-19 18:15:43 +0300

    Added a fallback for my backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import io from 'socket.io-client';
 
 // Base URLs
-const backendBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const backendBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 const API_BaseUrl = `${backendBaseUrl}/api`;
 
 // Axios instance
