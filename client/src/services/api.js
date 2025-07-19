a4b7f6ef834df16144beeec88e660c2b174c3176
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-19 18:02:44 +0300

    Added a connection to the backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import io from 'socket.io-client';
 
 // Base URLs
-const backendBaseUrl = "http://localhost:5000";
+const backendBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const API_BaseUrl = `${backendBaseUrl}/api`;
 
 // Axios instance
